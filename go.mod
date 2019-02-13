bf657e8f93636077fde89e82d1ec89ec1749a9ed
Hexilee <hexileee@gmail.com>
2019-02-13 21:35:17 +0800

    pass all tests

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/QSCTech/jw-scraper
+
+require (
+	github.com/spf13/viper v1.3.1
+	github.com/stretchr/testify v1.3.0
+	golang.org/x/text v0.3.0
+	google.golang.org/grpc v1.18.0
+)
---
+++ http.go
@@ -0,0 +1,249 @@
+package jw_scraper
+
+import (
+	"golang.org/x/text/encoding/simplifiedchinese"
+	"golang.org/x/text/transform"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+	"io/ioutil"
+	"net/http"
+	"net/url"
+	"path"
+	"strings"
+)
+
+type HttpService struct {
+	baseUrl             *url.URL
+	noRedirectionClient *http.Client
+}
+
+func NewHttpService(rawBaseUrl string) *HttpService {
+	baseUrl, err := url.Parse(rawBaseUrl)
+	if err != nil {
+		panic("invalid base URL: " + err.Error())
+	}
+
+	return &HttpService{
+		baseUrl: baseUrl,
+		noRedirectionClient: &http.Client{
+			CheckRedirect: func(req *http.Request, via []*http.Request) error {
+				return http.ErrUseLastResponse
+			},
+		},
+	}
+}
+
+func (service *HttpService) GetLoginPage() (page string, err error) {
+	resp, err := http.Get(service.baseUrl.String())
+	if err != nil {
+		return
+	}
+	defer func() {
+		if closeBodyErr := resp.Body.Close(); closeBodyErr != nil {
+			err = closeBodyErr
+		}
+	}()
+	data, err := ioutil.ReadAll(transform.NewReader(resp.Body, simplifiedchinese.GBK.NewDecoder())) // decoding from gbk is necessary
+	if err != nil {
+		return
+	}
+	page = string(data)
+	return
+}
+
+/**
+@Error: if err is kind of grpc.Status with code codes.InvalidArgument, username or password is invalid,
+		otherwise, a http error occurred
+*/
+func (service *HttpService) Login(username, password, viewState string) (jwbCookie string, err error) {
+	targetUrl := *service.baseUrl
+	targetUrl.Path = path.Join(targetUrl.Path, "default2.aspx")
+	req, err := http.NewRequest(
+		http.MethodPost,
+		targetUrl.String(),
+		strings.NewReader(
+			url.Values{
+				"__EVENTTARGET":    {"Button1"},
+				"__EVENTARGUMENT":  {""},
+				"__VIEWSTATE":      {viewState},
+				"TextBox1":         {username},
+				"TextBox2":         {password},
+				"RadioButtonList1": {"%D1%A7%C9%FA"}, // decoding in gbk: "学生"
+				"Text1":            {""},
+			}.Encode(),
+		),
+	)
+	if err != nil {
+		return
+	}
+
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	resp, err := service.noRedirectionClient.Do(req)
+	if err != nil {
+		return
+	}
+
+	if err = resp.Body.Close(); err != nil {
+		return
+	}
+
+	if resp.StatusCode == http.StatusFound {
+		for _, cookie := range resp.Cookies() {
+			if cookie.Name == "ASP.NET_SessionId" {
+				jwbCookie = cookie.Value
+				return
+			}
+		}
+	}
+
+	err = status.Error(codes.InvalidArgument, "authenticating fails")
+	return
+}
+
+func (service *HttpService) GetDefaultCourses(stuId, jwbCookie string) (page string, err error) {
+	targetUrl := *service.baseUrl
+	targetUrl.Path = path.Join(targetUrl.Path, "xskbcx.aspx")
+	finalQuery := targetUrl.Query()
+	finalQuery.Add("xh", stuId)
+	targetUrl.RawQuery = finalQuery.Encode()
+	req, err := http.NewRequest(http.MethodGet, targetUrl.String(), nil)
+	if err != nil {
+		return
+	}
+
+	req.AddCookie(&http.Cookie{
+		Name:  "ASP.NET_SessionId",
+		Value: jwbCookie,
+	})
+
+	return service.sendRequest(req)
+}
+
+func (service *HttpService) GetCourses(stuId, jwbCookie, schoolYear, semester, viewState, eventTarget string) (page string, err error) {
+	xqdEncoded, _, err := transform.String(simplifiedchinese.GBK.NewEncoder(), semester)
+	if err != nil {
+		panic(err)
+	}
+
+	targetUrl := *service.baseUrl
+	targetUrl.Path = path.Join(targetUrl.Path, "xskbcx.aspx")
+	finalQuery := targetUrl.Query()
+	finalQuery.Add("xh", stuId)
+	targetUrl.RawQuery = finalQuery.Encode()
+
+	req, err := http.NewRequest(
+		http.MethodPost,
+		targetUrl.String(),
+		strings.NewReader(
+			url.Values{
+				"__EVENTTARGET":   {eventTarget},
+				"__EVENTARGUMENT": {""},
+				"__VIEWSTATE":     {viewState},
+				"xxms":            {string([]byte{0xC1, 0xD0, 0xB1, 0xED})}, // decoding in gbk: "列表"
+				"xnd":             {schoolYear},
+				"xqd":             {xqdEncoded},
+				"kcxx":            {""},
+			}.Encode(),
+		))
+	if err != nil {
+		return
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded") // It's necessary
+	req.AddCookie(&http.Cookie{
+		Name:  "ASP.NET_SessionId",
+		Value: jwbCookie,
+	})
+	return service.sendRequest(req)
+}
+
+func (service *HttpService) GetDefaultExams(stuId, jwbCookie string) (page string, err error) {
+	targetUrl := *service.baseUrl
+	targetUrl.Path = path.Join(targetUrl.Path, "xskscx.aspx")
+	finalQuery := targetUrl.Query()
+	finalQuery.Add("xh", stuId)
+	targetUrl.RawQuery = finalQuery.Encode()
+
+	req, err := http.NewRequest(http.MethodGet, targetUrl.String(), nil)
+	if err != nil {
+		return
+	}
+	req.AddCookie(&http.Cookie{
+		Name:  "ASP.NET_SessionId",
+		Value: jwbCookie,
+	})
+
+	return service.sendRequest(req)
+}
+
+func (service *HttpService) GetExams(stuId, jwbCookie, schoolYear, semester, viewState, eventTarget string) (page string, err error) {
+	targetUrl := *service.baseUrl
+	targetUrl.Path = path.Join(targetUrl.Path, "xskscx.aspx")
+	finalQuery := targetUrl.Query()
+	finalQuery.Add("xh", stuId)
+	targetUrl.RawQuery = finalQuery.Encode()
+
+	xqdEncoded, _, err := transform.String(simplifiedchinese.GBK.NewEncoder(), semester)
+	if err != nil {
+		panic(err)
+	}
+
+	req, err := http.NewRequest(
+		http.MethodPost,
+		targetUrl.String(),
+		strings.NewReader(
+			url.Values{
+				"__EVENTTARGET":   {eventTarget},
+				"__EVENTARGUMENT": {""},
+				"__VIEWSTATE":     {viewState},
+				"xnd":             {schoolYear},
+				"xqd":             {xqdEncoded},
+			}.Encode(),
+		))
+	if err != nil {
+		return
+	}
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded") // It's necessary
+	req.AddCookie(&http.Cookie{
+		Name:  "ASP.NET_SessionId",
+		Value: jwbCookie,
+	})
+	return service.sendRequest(req)
+}
+
+func (service *HttpService) GetScores() (page string, err error) {
+	panic("not implemented")
+}
+
+func (service *HttpService) GetMajorScores() (page string, err error) {
+	panic("not implemented")
+}
+
+func (service *HttpService) GetTotalCredit() (page string, err error) {
+	panic("not implemented")
+}
+
+func (service *HttpService) sendRequest(req *http.Request) (page string, err error) {
+	resp, err := service.noRedirectionClient.Do(req)
+	if err != nil {
+		return
+	}
+
+	if resp.StatusCode == http.StatusFound {
+		err = status.Error(codes.Unauthenticated, "invalid cookie")
+		return
+	}
+
+	defer func() {
+		if closeBodyErr := resp.Body.Close(); err == nil && closeBodyErr != nil {
+			err = closeBodyErr
+			return
+		}
+	}()
+
+	data, err := ioutil.ReadAll(transform.NewReader(resp.Body, simplifiedchinese.GBK.NewDecoder()))
+	if err != nil {
+		return
+	}
+	page = string(data)
+	return
+}
