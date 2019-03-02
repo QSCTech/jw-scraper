e25f3504d55168112d7a284c7369e2c0b3bc44f2
Hexilee <hexileee@gmail.com>
2019-03-02 10:29:37 +0800

    update interface HttpService

--- service.go
+++ service.go
@@ -13,9 +13,11 @@ type HttpService interface {
 
 	GetExams(stuId, jwbCookie, schoolYear, semester, viewState, eventTarget string) (page string, status int, err error)
 
-	GetScores() (page string, status int, err error)
+	GetScoresBase(stuId, jwbCookie string) (page string, status int, err error)
+
+	GetScores(stuId, jwbCookie, schoolYear, viewState string) (page string, status int, err error)
 
 	GetMajorScores(stuId, jwbCookie string) (page string, status int, err error)
 
-	GetTotalCredit() (page string, status int, err error)
+	GetTotalCredit(stuId, jwbCookie string) (page string, status int, err error)
 }
